198d53e2006ee7411b3a60eb871eaf3d9ce692a8
AdamSimpson <adambsimpson@gmail.com>
2016-08-15 17:28:58 -0400

    fixed bug in texture cache

--- src/Rendering/texture_cache.cpp
+++ src/Rendering/texture_cache.cpp
@@ -3,9 +3,9 @@
 sf::Texture& TextureCache::get(const std::string& filename) {
   const auto pair = cache_.emplace(filename, std::make_unique<sf::Texture>());
   sf::Texture& texture = *(pair.first->second);
-  const bool exists = pair.second;
+  const bool inserted = pair.second; // true if new texture inserted into cache
 
-  if(!exists)
+  if(inserted)
     texture.loadFromFile(filename);
 
   return texture;
